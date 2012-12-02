9b633b5296a3edaae73bdff57583b737664d6abc
Jason Altekruse <altekrusejason@open-math.com>
2012-12-01 19:55:36 -0600

    moved more stuff out of web root, adding new database schema.

--- database/openmath_database_12_01_12.sql
+++ database/openmath_database_12_01_12.sql
@@ -8,6 +8,8 @@ USE `openmath` ;
 -- -----------------------------------------------------
 -- Table `openmath`.`schools`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`schools` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`schools` (
   `school_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(50) NOT NULL ,
@@ -20,6 +22,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `openmath`.`classes`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`classes` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`classes` (
   `class_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
   `subject` VARCHAR(50) NULL DEFAULT NULL ,
@@ -43,6 +47,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `openmath`.`user_profiles`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`user_profiles` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`user_profiles` (
   `user_id` INT(11) NOT NULL AUTO_INCREMENT ,
   `id` INT(11) NULL DEFAULT NULL ,
@@ -58,6 +64,8 @@ DEFAULT CHARACTER SET = dec8;
 -- -----------------------------------------------------
 -- Table `openmath`.`documents`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`documents` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`documents` (
   `doc_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
   `upload_date` DATETIME NOT NULL ,
@@ -96,11 +104,13 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `openmath`.`comments`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`comments` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`comments` (
-  `comment_id` INT NOT NULL ,
+  `comment_id` INT NOT NULL AUTO_INCREMENT ,
   `title` VARCHAR(100) NULL ,
   `description` VARCHAR(500) NULL ,
-  `doc_id` INT(10) UNSIGNED NOT NULL ,
+  `doc_id` INT(10) UNSIGNED NULL ,
   `post_date` DATETIME NULL ,
   `user_id` INT(11) NOT NULL ,
   PRIMARY KEY (`comment_id`) ,
@@ -122,6 +132,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `openmath`.`assignments`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`assignments` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`assignments` (
   `assignment_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
   `assign_date` DATETIME NULL DEFAULT NULL ,
@@ -156,6 +168,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `openmath`.`assignments_and_users`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`assignments_and_users` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`assignments_and_users` (
   `assignment_id` INT(10) UNSIGNED NOT NULL ,
   `user_id` INT(11) NOT NULL ,
@@ -188,6 +202,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `openmath`.`class_doc_sharing`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`class_doc_sharing` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`class_doc_sharing` (
   `doc_id` INT(10) UNSIGNED NOT NULL ,
   `class_id` INT(10) UNSIGNED NOT NULL ,
@@ -213,6 +229,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `openmath`.`class_enrollments`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`class_enrollments` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`class_enrollments` (
   `class_id` INT(10) UNSIGNED NOT NULL ,
   `start_date` DATE NULL DEFAULT NULL ,
@@ -233,6 +251,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `openmath`.`class_members`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`class_members` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`class_members` (
   `class_id` INT(10) UNSIGNED NOT NULL ,
   `user_id` INT(11) NOT NULL ,
@@ -257,8 +277,10 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `openmath`.`tag_radix_nodes`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`tag_radix_nodes` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`tag_radix_nodes` (
-  `node_id` INT NOT NULL ,
+  `node_id` INT NOT NULL AUTO_INCREMENT ,
   `character` CHAR(1) NULL ,
   `depth` INT UNSIGNED NULL ,
   PRIMARY KEY (`node_id`) )
@@ -268,6 +290,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `openmath`.`tags`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`tags` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`tags` (
   `tag` VARCHAR(50) NULL DEFAULT NULL ,
   `tag_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
@@ -290,6 +314,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `openmath`.`document_tags`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`document_tags` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`document_tags` (
   `tag_id` INT(10) UNSIGNED NOT NULL ,
   `doc_id` INT(10) UNSIGNED NOT NULL ,
@@ -313,6 +339,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `openmath`.`colleagues`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`colleagues` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`colleagues` (
   `sender_user_id` INT(11) NOT NULL ,
   `receiver_user_id` INT(11) NOT NULL ,
@@ -336,6 +364,8 @@ DEFAULT CHARACTER SET = hp8;
 -- -----------------------------------------------------
 -- Table `openmath`.`school_doc_sharing`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`school_doc_sharing` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`school_doc_sharing` (
   `doc_id` INT(10) UNSIGNED NOT NULL ,
   `school_id` INT(10) UNSIGNED NOT NULL ,
@@ -361,6 +391,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `openmath`.`schools_and_users`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`schools_and_users` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`schools_and_users` (
   `school_id` INT(10) UNSIGNED NOT NULL ,
   `user_id` INT(11) NOT NULL ,
@@ -385,6 +417,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `openmath`.`user_doc_sharing`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`user_doc_sharing` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`user_doc_sharing` (
   `user_id` INT(11) NOT NULL ,
   `doc_id` INT(10) UNSIGNED NOT NULL ,
@@ -410,6 +444,8 @@ DEFAULT CHARACTER SET = dec8;
 -- -----------------------------------------------------
 -- Table `openmath`.`doc_revision_edges`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`doc_revision_edges` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`doc_revision_edges` (
   `ancestor` INT(10) UNSIGNED NOT NULL ,
   `descendant` INT(10) UNSIGNED NOT NULL ,
@@ -434,6 +470,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `openmath`.`comment_edges`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`comment_edges` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`comment_edges` (
   `ancestor` INT NOT NULL ,
   `descendant` INT NOT NULL ,
@@ -457,6 +495,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `openmath`.`doc_ratings`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`doc_ratings` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`doc_ratings` (
   `doc_id` INT(10) UNSIGNED NOT NULL ,
   `user_id` INT(11) NOT NULL ,
@@ -483,6 +523,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `openmath`.`ci_sessions`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`ci_sessions` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`ci_sessions` (
   `session_id` VARCHAR(40) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '0' ,
   `ip_address` VARCHAR(16) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '0' ,
@@ -498,6 +540,8 @@ COLLATE = utf8_bin;
 -- -----------------------------------------------------
 -- Table `openmath`.`login_attempts`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`login_attempts` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`login_attempts` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `ip_address` VARCHAR(40) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
@@ -513,6 +557,8 @@ COLLATE = utf8_bin;
 -- -----------------------------------------------------
 -- Table `openmath`.`user_autologin`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`user_autologin` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`user_autologin` (
   `key_id` CHAR(32) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
   `user_id` INT(11) NOT NULL DEFAULT '0' ,
@@ -528,6 +574,8 @@ COLLATE = utf8_bin;
 -- -----------------------------------------------------
 -- Table `openmath`.`users`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`users` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`users` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `username` VARCHAR(50) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
@@ -554,6 +602,8 @@ COLLATE = utf8_bin;
 -- -----------------------------------------------------
 -- Table `openmath`.`tag_radi_edges`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `openmath`.`tag_radi_edges` ;
+
 CREATE  TABLE IF NOT EXISTS `openmath`.`tag_radi_edges` (
   `ancestor` INT NOT NULL ,
   `descendant` INT NOT NULL ,
